29820e3ce1b54586df252ad95bf2dc21fff196d2
ABHIRAM RAVULA <it22737055@matrusri.edu.in>
2025-04-27 15:02:22 +0000

    L27.cpp done L28 was an installation in mac so done too

--- L27.cpp
+++ L27.cpp
@@ -45,6 +45,19 @@ front , back
 • empty
 */
 
+
+
+bool comparator(pair<int, int> p1 , pair<int, int> p2){
+  if (p1.second < p2.second) return true;  
+  if (p1.second > p2.second) return false;  
+  
+  if(p1.first < p2.first) return true;
+  else return false;
+}
+
+
+
+
 int main(){
 
     // vector<int> v1; // vector of integers
@@ -132,26 +145,26 @@ list<int> I = {1, 2, 3};
 //rend, insert, front, back
   */
 
-  list<int> l1 = {1, 2, 3, 4, 5};
-  list<int> l ;
-  l.emplace_back(1);
-    l.push_back(20);
-    l.push_back(30);
-    l.push_front(40);
-    l.push_front(50);
+//   list<int> l1 = {1, 2, 3, 4, 5};
+//   list<int> l ;
+//   l.emplace_back(1);
+//     l.push_back(20);
+//     l.push_back(30);
+//     l.push_front(40);
+//     l.push_front(50);
 
-    for(int val:l){
-        cout << val << " ";
-    }
-    cout << endl;
+//     for(int val:l){
+//         cout << val << " ";
+//     }
+//     cout << endl;
     
-    l.pop_back();
-    l.pop_front();
+//     l.pop_back();
+//     l.pop_front();
 
-    for(int val:l){
-        cout << val << " ";
-    }
-    cout << endl;
+//     for(int val:l){
+//         cout << val << " ";
+//     }
+//     cout << endl;
 
 /*
 Deque : Double Ended Queue NOT dequeue
@@ -163,26 +176,364 @@ deque<int> d = {1, 2, 3};
 rend, insert, front, back
 */
 
-    deque<int> d = {1, 2, 3};
-    d.push_back(4);
-    d.push_front(0);
-    d.emplace_back(5);
-    d.emplace_front(-1);
-    for(int val:d){
-        cout << val << " ";
-    }
-    cout << endl;
-    d.pop_back();
-    d.pop_front();
-    for(int val:d){
-        cout << val << " ";
-    }
+    // deque<int> d = {1, 2, 3};
+    // d.push_back(4);
+    // d.push_front(0);
+    // d.emplace_back(5);
+    // d.emplace_front(-1);
+    // for(int val:d){
+    //     cout << val << " ";
+    // }
+    // cout << endl;
+    // d.pop_back();
+    // d.pop_front();
+    // for(int val:d){
+    //     cout << val << " ";
+    // }
 
 
+    /*
+    Pair
+pair<int, int> p = { 3, 5 };
+pair<char, int> p = { 'a', 1 };
+    */
+
+    // pair<int, int> p0 = { 3, 5 };
+    // cout << p0.first << " " << p0.second << endl;
+    // pair<int, int> p1 = make_pair(1, 2);
+    // cout << p1.first << " " << p1.second << endl;
+    
+    // pair<int , pair<char, int>> p = {1, {'a',3}};
+    // cout << p.first << " " << p.second.first << " " << p.second.second << endl;
+    // cout << p.second.first << " " << p.second.second << endl;
 
 
+    // vector<pair<int , int>> vec = {{1,2},{2,3},{3,4}};
+
+    // for(pair<int, int> p : vec){
+    //     cout << p.first << " " << p.second << endl;
+    // }
+    // cout << endl;
+    
+    // vec.push_back({4,5});//insert at end
+    // vec.emplace_back(5,6); //in-place objects cteate
+
+    // for(auto p : vec){
+    //     cout << p.first << " " << p.second << endl;
+    // }
+    // cout << endl;
+/*
+Stack
+stack<int> s;
+push, emplace
+top
+pop
+size
+empty
+swap
+*/
+
+//  stack<int> s;
+//  s.push(1);
+//     s.push(2);
+//     s.push(3);
+//     cout << s.top() << endl; 
+//     cout << s.size() << endl; 
+//     cout << s.empty() << endl;
+
+//     stack<int> s2;
+//     s2.swap(s); // swap two stacks
 
 
+//     while (!s.empty()) {
+//         cout << s.top() << " ";
+//         s.pop();
+//     }
+//     cout << endl;
+    
+   
+//     cout << s.size() << endl; 
+//     cout << s2.size() << endl; 
+
+  /*
+   Queue
+queue<int> q;
+push, emplace
+front
+pop
+Size
+empty
+swap
+  
+  */
+   
+    // queue<int> q;
+    // q.push(1);
+    // q.push(2);
+    // q.push(3);
+    // cout << q.front() << endl;
+    // cout << q.size() << endl;
+    // cout << q.empty() << endl;
+    // queue<int> q2;
+    // q2.swap(q); // swap two queues
+    // while (!q.empty()) {
+    //     cout << q.front() << " ";
+    //     q.pop();
+    // }
+    // cout << endl;
+    // cout << q.size() << endl;
+    // cout << q2.size() << endl;
+    //     while (!q2.empty()) {
+    //     cout << q2.front() << " ";
+    //     q2.pop();
+    // }
+
+   /*
+   Priority Queue (mac heap / min heap)
+    priority_queue<int> pq;
+    priority_queue<int, vector<int>, greater<int>> q;
+push, emplace
+top
+pop
+size
+empty
+   */
+    
+//    priority_queue<int> pq; // max heap
+// //   priority_queue<int, vector<int>, greater<int>> pq ;
+//     pq.push(5);
+//     pq.push(0);
+//     pq.push(1);
+//     pq.push(2);
+//     pq.push(3);
+//     cout << pq.top() << endl; // top element
+//     cout << pq.size() << endl; // size of queue
+//     cout << pq.empty() << endl; // check if queue is empty
+
+//     while (!pq.empty()) {
+//         cout << pq.top() << " ";
+//         pq.pop();
+//     }
+//     cout << endl;
+
+//     cout << pq.size() << endl; // size of queue
+
+//     priority_queue<int, vector<int>, greater<int>> pq2;
+
+   /*
+Map  (key, value)
+map<string, int> m;
+m[key] = value;
+
+sorted (ascending order)
+
+insert, emplace
+count
+erase
+find
+size, empty, erase
+   */
+
+  //  map<string, int> m ;
+
+  //  m["tv"] = 100;
+  //  m["laptop"] = 100;
+  //  m["headphone"] = 100;
+   
+  //  m.insert({"camera", 50});
+  //  m.emplace(make_pair("phone", 25));
+
+  //  m.erase("tv");
+
+  //   for(auto p:m){
+  //     cout << p.first<<" "<<p.second<<endl;
+  //   }
+  //   cout << endl;
+    
+  //  cout << "count =" << m.count("laptop") << " "<< "value: "<< m["laptop"] << endl;
+   
+  //    if(m.find("camera") != m.end()){
+  //     cout << "found " ;
+  //    }  else {
+  //     cout << "not found ";
+  //    }
+  //     cout << endl;
+     
+      /*
+      Other Maps
+• Multi Map
+multimap<string, int> m;
+• Unordered Map
+unordered_mop<string, int> m;
+      */
+
+      // multimap<string, int> m1;
+      // m1.insert({"tv", 100});
+      // m1.insert({"tv", 200});
+      // m1.insert({"tv", 300});
+      // m1.insert({"tv", 400});
+      // // m1.erase("tv");
+      // m1.erase(m1.find("tv"));
+     
+      // for(auto p:m1){
+      //   cout << p.first << " " << p.second << endl;
+      // }
+      // cout << endl;
+
+      // unordered_map<string, int> m2;
+      // m2["tv"] = 100;
+      // m2["laptop"] = 200;
+      // m2["headphone"] = 300;
+      // m2["camera"] = 400;
+      // m2["phone"] = 500;
+      // m2["tv"] = 1000;
+      // m2.erase("tv");
+      // m2.erase(m2.find("laptop"));
+      // for(auto p:m2){
+      //   cout << p.first << " " << p.second << endl;
+      // }
+      // cout << endl;
+
+
+     /*
+     Set
+ set<int> s;
+ insert, emplace
+ count
+ erase
+ find
+ size, empty, erase
+     */
+
+    // set <int> s;
+
+    // s.insert(1);
+    // s.insert(2);
+    // s.insert(3);
+    // s.insert(4);
+    // s.insert(5);
+
+    // s.insert(4);
+    // s.insert(5);
+
+    // cout << "size: " << s.size() << endl;
+
+    // for(auto val:s){
+    //     cout << val << " ";
+    // }
+    // cout << endl;
+
+    // lower_bound(s.begin(), s.end(), 3); // returns iterator to first element greater than or equal to 3
+    // upper_bound(s.begin(), s.end(), 3); // returns iterator to first element greater than 3
+    // cout << "lower_bound: " << *lower_bound(s.begin(), s.end(), 3) << endl;
+    // cout << "upper_bound: " << *upper_bound(s.begin(), s.end(), 3) << endl;
+    
+//     set<int> s1;
+// s1. insert(1);
+// s1. insert(2);
+// s1. insert(3) ;
+// s1. insert(4) ;
+// s1. insert(5);
+// cout<< "lower bound = "<< *(s1.lower_bound(4)) << endl;
+// cout << "upper bound = " << *(s1.upper_bound(4)) << endl;
+
+// for(auto val : s1) {
+// cout << val << " ";
+// }cout << endl;
+
+/*
+Other Sets
+• Multi Set
+multiset<int> s;
+• Unordered Set
+unordered_set<int> s;
+
+*/
+
+/*
+
+    Algorithms
+• Sorting
+sort(arr, arr+n)
+sort(arr, arr+n, greater<int>())
+sort(v.begin(), v,end())
+
+*/
+vector<int> vec = {43,22,33,11,2,67,88,97} ;
+sort (vec. begin(),vec. end());
+for(int val : vec) {
+cout<< val << " " ;
+}
+cout << endl;
+ 
+sort (vec.begin(),vec.end(),greater<int>());
+for(int val : vec) {
+cout<< val << " " ;
+}
+cout << endl;
+
+
+vector<pair<int,int>> vec1 = {{10,2},{2,3},{3,4},{3,4}};
+sort (vec1. begin(),vec1. end(), comparator); // sort by second element custom function is written above the int main
+for(auto p : vec1) {
+cout<< p.first  << " " << p.second << endl ;
+}
+
+/*
+Algorithms
+• Reverse
+reverse(v.begin(), v.end())
+• Next Permutation
+next_permutotion(v.begin(), v.end())
+• swap, min, max
+
+  Max & Min Element
+max_element(v.begin(), v.end())
+min_element(v.begin(), v.end())
+  Binary Search
+binary_search(v.begin(), v.end(), target)
+   Count Set Bits
+_builtin_popcount( )
+_builtin_popcount/( )
+_builtin_popcount/l( )
+
+*/
+
+//  vector<int> vec2 = {1,2,3,4,5};
+//   reverse(vec2.begin()+1, vec2.begin()+3);
+
+//   for(auto val : vec2) {
+//     cout << val << " ";
+//   }
+//   cout << endl;
+
+string s = "bca";
+prev_permutation(s.begin(), s.end());
+cout << s << endl;
+
+vector<int> vec45 = {1, 2, 3,32,42,34,23, 4, 5};
+int a=9 , b=6;
+swap(a, b);
+cout << "min: " << min(1, 2) << endl;
+cout << "max: " << max(1, 2) << endl;
+
+
+cout << *(max_element(vec45.begin(), vec45.end())) << endl;
+
+
+cout << *(min_element(vec45.begin(), vec45.end())) << endl;
+
+
+sort(vec45.begin() , vec45.end());
+cout << "binary search: " << binary_search(vec45.begin(), vec45.end(), 12) << endl;
+
+int n =15; 
+long int n1 =15;
+long long int n2 = 15;
+
+cout << "count set bits: " << __builtin_popcount(n) << endl;
+cout << "count set bits: " << __builtin_popcountl(n1) << endl;
+cout << "count set bits: " << __builtin_popcountll(n2) << endl;
 
 
 
