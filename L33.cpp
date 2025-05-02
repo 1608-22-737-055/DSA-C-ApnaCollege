83d04289cd788e8166fc014da6a4cb37eb1f2d12
ABHIRAM RAVULA <it22737055@matrusri.edu.in>
2025-05-02 04:47:31 +0000

    updated

---
+++ L33.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+//  Euclid's Algorithm | Sieve of Eratosthenes | Modular Arithmetics
+
+
+/*
+What we'll cover...
+• Prime Logic & Sieve of Eratosthenes
+• GCD with Euclid's Algorithm & LCM
+• Digits in a number
+• Reverse a num
+• Check Palindrome
+• Armstrong Logic
+• Modular Arithmetics
+• Fast Exponentiation
+*/
+int main(){
+
+
+    return 0;
+}
