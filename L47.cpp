0cc4d5e60dcf335f8967b288b4a58f63ee95fa86
ABHIRAM RAVULA <it22737055@matrusri.edu.in>
2025-05-15 17:43:24 +0000

    L47,48

---
+++ L47.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+// #include<unordered_map>
+// #include<unordered_set>
+// #include<queue>
+// #include<stack>     
+
+
+using namespace std;
+
+// Rat in a maze
+
+void helper( vector<vector<int>> &mat, int r , int c, string path,
+     vector<string> &ans ){
+    int n = mat.size();
+
+    if(r < 0 || c<0 || r>=n || c>=n || mat[r][c]==0  || mat[r][c] == -1 ){
+        return; 
+    }
+
+    if(r==n-1 && c==n-1){
+        ans.push_back(path);
+        return;
+    }
+
+    mat[r][c]= -1;
+
+    helper(mat, r+1, c, path + 'D', ans); // down
+    helper(mat, r, c+1, path + 'R', ans); // right
+    helper(mat, r-1, c, path + 'U', ans); // up
+    helper(mat, r, c-1, path + 'L', ans); // left
+
+    mat[r][c]= 1;
+
+}
+
+vector<string> findPath(vector<vector<int>> &mat){
+   int n= mat.size();
+  
+    vector<string> ans;
+   string path = "";
+   vector<vector<bool>> vis(n, vector<bool>(n, false)); 
+
+   helper(mat, 0 , 0, path, ans );
+
+   return ans;
+ }
+
+int main(){
+    vector<vector<int>> mat = {
+        {1, 0, 0, 0},
+        {1, 1, 0, 1},
+        {1, 1, 0, 0},
+        {0, 1, 1, 1}
+    };
+
+    vector<string> ans = findPath(mat);
+    for(string path: ans){
+        cout << path << " " << endl;
+    }
+
+    return 0;
+}
