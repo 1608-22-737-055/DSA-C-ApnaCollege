4308faba7208f4b9379bb9aecb31f13dacd761a1
ABHIRAM RAVULA <it22737055@matrusri.edu.in>
2025-05-16 05:10:49 +0000

    49 and 50 done

---
+++ L49.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<algorithm>
+
+#include<cmath>
+#include<limits>
+#include<set>
+
+
+using namespace std;
+
+//LC-131 Palindrome Partitioning
+/*
+class Solution {
+public: 
+
+      bool isPalin(string s){
+            string s2 = s ;
+            reverse(s2.begin(),s2.end());
+            return s == s2;
+          } 
+
+
+      void getAllParts(string s, vector<string> &partitions, vector<vector<string>> &ans){
+
+          if(s.size() == 0){
+            ans.push_back(partitions);
+            return;
+          }
+
+          for(int i=0; i<s.size(); i++){
+            string part = s.substr(0, i+1);
+
+            if(isPalin(part)){
+                partitions.push_back(part);
+                getAllParts(s.substr(i+1), partitions, ans);
+                partitions.pop_back();
+            }
+          }
+      }
+
+      
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> partitions;
+
+        getAllParts(s, partitions, ans);
+
+        return ans;
+    }
+};
+*/
+
+int main(){
+
+    string s = "aab";
+    int n = s.length();
+
+    return 0;
+}
